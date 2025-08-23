3f149abc8327d252ae4d4c731709003990fb816c
AntikSRO <genpotv@gmail.com>
2025-08-23 22:52:25 +0200

    Update constants.ts
    Signed-off-by: AntikSRO <genpotv@gmail.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_PATH = import.meta.env.VITE_API_URL || "play.petkopije.beer:25569" // <- Edit this string to your own API
-export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.ree6.de" // <- Edit this string to your Invite link
+export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.petkopije.beer" // <- Edit this string to your Invite link
 
 export function get(path: string) {
 
